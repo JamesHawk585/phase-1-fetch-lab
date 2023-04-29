21a66b6a8ba72701346b059bf338866aee4a485d
James Hawk <jph94880@gmail.com>
2023-04-29 15:05:29 -0500

    Adds my file.md

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+
+// const main = document.querySelector('main');
+
+// fetch("https://anapioficeandfire.com/api/books")
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     // Use the data from the response to do DOM manipulation
+//     // document.createAttribute(data)
+//     main.setAttribute(data);
+//   });
   
+
+function fetchBooks() {
+ return fetch("https://anapioficeandfire.com/api/books")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    renderBooks(data);
+  });
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +35,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+module.exports = { fetchBooks, renderBooks };
